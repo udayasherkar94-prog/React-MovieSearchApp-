590b6a1893efecb93863f1c5d8220bee66838990
udayasherkar94-prog <udayasherkar94@gmail.com>
2026-06-22 22:56:55 +0530

    modification of components through functions and props,sates from here App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from './Components/Navbar'
 import Movies from './Components/Movies'
 import Watchlist from './Components/Watchlist'
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, useSearchParams} from 'react-router-dom'
 import Banner from './Components/Banner'
 
-function App() {
+function App({movieObj}) {
+
+  let [watchlist,setWatchlist] = useState([]);
+
+  let addMovies =(movieObj)=>{
+     let newWatchlist = [...watchlist,movieObj]
+     setWatchlist(newWatchlist);
+     localStorage.setItem("movieList",JSON.stringify(newWatchlist));
+     console.log(newWatchlist)
+  }
+
+  let removeMovies = (movieObj)=>{
+    let filteredWatchlist = watchlist.filter((movie)=>{
+      return movie.id != movieObj.id;
+    })
+
+    setWatchlist(filteredWatchlist);
+    localStorage.setItem("movieList",JSON.stringify(filteredWatchlist));
+    console.log(filteredWatchlist)
+  }
+
+  useEffect(()=>{
+    let localStorageValue = localStorage.getItem('movieList');
+    if(!localStorageValue){
+      return;
+    }
+    setWatchlist(JSON.parse(localStorageValue));
+  },[])
+
   return (
     <> 
     
@@ -14,9 +42,9 @@ function App() {
         <Navbar/> 
 
         <Routes>
-             <Route path="/" element={ <> <Banner /> <Movies /> </>}></Route>
+             <Route path="/" element={ <> <Banner /> <Movies watchlist={watchlist} removeMovies={removeMovies}  addMovies={addMovies} movieObj={movieObj} /> </>}></Route>
 
-              <Route path="/watchlist" element={<Watchlist />}></Route>
+              <Route path="/watchlist" element={<Watchlist watchlist={watchlist} removeMovies={removeMovies} />}></Route>
 
         </Routes>
      </BrowserRouter>
